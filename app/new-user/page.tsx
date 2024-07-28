ba18103b110c143765b55ce880c685652145e15f
Nepsoul <nirumagar901@gmail.com>
2024-07-29 05:22:06 +0545

    chore: creating new user and re-locate db to root folder

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,4 +1,28 @@
-const NewUser = () => {
-  return <div>Hi</div>;
+import { prisma } from "@/utils/db";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const createNewUser = async () => {
+  // const {userId} =await auth()
+  const user = await currentUser();
+
+  const match = await prisma.user.findUnique({
+    where: { clerkId: user.id as string },
+  });
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user?.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+  redirect("/journal");
+};
+
+const NewUser = async () => {
+  await createNewUser();
+
+  return <div>....loading</div>;
 };
 export default NewUser;
