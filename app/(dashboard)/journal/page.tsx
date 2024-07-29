6bd3a8f49156a42f6f03e009e8b905d137918f39
Nepsoul <nirumagar901@gmail.com>
2024-07-29 07:19:16 +0545

    chore: new journal entry ui

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,7 @@
 import { prisma } from "@/utils/db";
 import { getUserByClerkId } from "@/utils/auth";
+import NewEntryCard from "@/components/NewEntryCard";
+import EntryCard from "@/components/entrycard";
 
 const getEntries = async () => {
   const user = await getUserByClerkId();
@@ -17,7 +19,18 @@ const getEntries = async () => {
 const JournalPage = async () => {
   const entries = await getEntries();
   console.log("entries", entries);
-  return <div>journal</div>;
+  return (
+    <div className="p-10 bg-zinc-100/50 h-full">
+      <h2 className="text-3xl mb-8">Journal</h2>
+      <div className="grid grid-cols-3 gap-4 ">
+        <NewEntryCard />
+
+        {entries.map((entry) => (
+          <EntryCard key={entry.id} entry={entry} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default JournalPage;
