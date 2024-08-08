b68b3fb051c2cd0b6234916d2d4865cc942ff352
Nepsoul <nirumagar901@gmail.com>
2024-08-08 14:07:45 +0545

    chore: autosaving entries feat. react-autosave

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -2,8 +2,8 @@ import { getUserByClerkID } from "@/utils/auth";
 import { prisma } from "@/utils/db";
 import { NextResponse } from "next/server";
 
-export const PATCH = async (request, { params }) => {
-  const { content } = request.json();
+export const PATCH = async (request: Request, { params }) => {
+  const { content } = await request.json();
   const user = await getUserByClerkID();
   const updatedEntry = await prisma.journalEntry.update({
     where: {
